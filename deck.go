fd17b2451fe0a1e27782bd4d524e20ce8d90802a
matsunaga-kenta1 <matsunaga-kenta1@dmm.com>
2024-11-19 15:38:32 +0900

    add saveToFile

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -31,4 +35,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666) // 0666 はパーミッション
 }
--- main.go
+++ main.go
@@ -4,9 +4,5 @@ func main() {
 	// := は新変数を定義(初期化)するときにのみ使用. func 内で使用すること.
 	// 既存の変数に新しい値を代入する場合は、= でよい
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
