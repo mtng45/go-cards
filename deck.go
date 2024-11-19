e671a09cb2b1f4582be247566404631e56505dde
matsunaga-kenta1 <matsunaga-kenta1@dmm.com>
2024-11-19 16:03:10 +0900

    add newDeckFromFile

--- deck.go
+++ deck.go
@@ -44,3 +44,13 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666) // 0666 はパーミッション
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	// := は新変数を定義(初期化)するときにのみ使用. func 内で使用すること.
 	// 既存の変数に新しい値を代入する場合は、= でよい
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
